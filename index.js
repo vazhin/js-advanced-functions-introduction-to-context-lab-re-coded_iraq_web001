524ba26eaf16646c5cadcb650dae95c31c1a7f07
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-09 18:21:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function createTimeInEvent(employeeRecordObj, dateTimeString) {
     type: "TimeIn",
     date: date,
     hour: Number(hour)
-  })
+  }
   employeeRecordObj.
   return employeeRecordObj.timeInEvents.push(timeInEvents)
 }
@@ -39,7 +39,7 @@ function createTimeOutEvent(employeeRecordObj, dateTimeString) {
     type: "TimeOut",
     date: date,
     hour: Number(hour)
-  })
+  }
   return employeeRecordObj
 }
 
